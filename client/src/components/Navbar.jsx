38494f270a599da60c76fa58602c5dd455f997c5
Adam SOudani <asoudani@email.com>
2020-06-22 22:15:55 -0700

    navbar and jumbotron

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Google Books</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Saved</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar
