94b55573a016610bc53aae5778f8acb398f6ac3c
Adam SOudani <asoudani@email.com>
2020-07-03 15:13:51 -0700

    heroku postbuild

---
+++ client/build/precache-manifest.bb5d0faff00799e5ad3918e75f2a44ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae2b0e53fee0cc541f8c",
+    "url": "/static/css/main.1ff441ad.chunk.css"
+  },
+  {
+    "revision": "ae2b0e53fee0cc541f8c",
+    "url": "/static/js/main.b5575e6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6cf179756dc2e13b790",
+    "url": "/static/js/2.22d26ffa.chunk.js"
+  },
+  {
+    "revision": "74fde231fa173766562f057662288b54",
+    "url": "/index.html"
+  }
+];
